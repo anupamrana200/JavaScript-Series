68efd1a1ea32505f8023c515e63c0be70380b2b6
Anupam Rana <141600262+anupamrana200@users.noreply.github.com>
2024-04-17 07:35:44 +0000

    learn datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all javaScript code as newer version
+ //alert(3+3) //we are using node.js not browser
+
+ let name = "Anupam"
+ let age = 23
+ let isLoggedIn = false
+
+ //Datatypes
+ //number => 2 to power 53
+ //string
+ //boolean
+ //null => standalone value
+ //undefined 
+ //symbol
+ //object
+
+ console.log(typeof null) //object type
+ console.log(typeof undefined) //undefined type
+
